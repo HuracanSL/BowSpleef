0ceab4cbb6e356c7660712c7065b8262a621f19b
HuracanSL <aeroxenogaming@gmail.com>
2015-09-18 16:21:09 +0800

    Update ArenaJoinEvent.java

--- ArenaJoinEvent.java
+++ ArenaJoinEvent.java
@@ -5,13 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-/**
- * Copyright HuracanSL (c) 2014. All Rights Reserved.
- * Any code contained within this document, and any associated APIs with similar brandings
- * are the sole property of HuracanSL. Distribution, reproduction, taking snippits, or
- * claiming any contents as your own will break the terms of the license, and void any
- * agreements with you, the third party.
- */
 public class ArenaJoinEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
